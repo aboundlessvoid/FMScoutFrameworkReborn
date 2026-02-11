dc50c3098a793d50ed8d2c2e6eff365b39c9e53e
aboundlessvoid <drunkonschadenfreude@protonmail.com>
2026-02-11 13:51:13 +0100

    Load Players with Staff roles

--- Entities/Ingame/Persons/PlayerStaff.cs
+++ Entities/Ingame/Persons/PlayerStaff.cs
@@ -4,14 +4,23 @@ using FMScoutFramework.Core.Entities.InGame.Interfaces;
 
 namespace FMScoutFramework.Core.Entities.InGame
 {
-	public class PlayerStaff : BaseObject, IPlayerStaff
+	public class PlayerStaff : BaseObject
 	{
+		public Player playerObject;
+		public Staff staffObject;
+
 		public PlayerStaff (Int64 memoryAddress, IVersion version) 
 			: base(memoryAddress, version)
-		{	}
+		{
+			playerObject = new Player(memoryAddress, version);
+			staffObject = new Staff(memoryAddress, version);
+		}
 		public PlayerStaff (Int64 memoryAddress, ArraySegment<byte> originalBytes, IVersion version) 
 			: base(memoryAddress, originalBytes, version)
-		{	}
+		{
+			playerObject = new Player(memoryAddress, originalBytes, version);
+            staffObject = new Staff(memoryAddress, originalBytes, version);
+		}
 	}
 }
 
